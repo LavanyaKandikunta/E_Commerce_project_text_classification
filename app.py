f9ab1a93a7c3f0d15f6d3a8c86bd098ec7b3cfea
LavanyaKandikunta <kandikuntalavanya@gmail.com>
2026-01-12 07:42:22 +0000

    app.py has 15th update Changed the port number

--- app.py
+++ app.py
@@ -179,8 +179,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 # ============================================================
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))  # Render dynamically assigns a port
+    app.run(host="0.0.0.0", port=port, debug=False)
 
 
 # In[ ]:
